6a54474c2a02d3964c901c1a0008a570b4540c57
23MinL <posing1606@gmail.com>
2025-03-30 04:56:34 +0900

    [fix] Code

--- src/main/java/org/example/booklog/BookLogApplication.java
+++ src/main/java/org/example/booklog/BookLogApplication.java
@@ -7,10 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookLogApplication {
     public static void main(String[] args) {
+        // 현재 작업 디렉토리에서 .env 파일을 찾고, 없으면 무시합니다.
         Dotenv dotenv = Dotenv.configure()
-                .directory("src/main/resources")
+                .directory(".")
+                .ignoreIfMissing()  // .env 파일이 없으면 예외 발생을 방지
                 .load();
 
+        // .env 파일의 내용이 있으면 시스템 프로퍼티로 설정합니다.
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 
         SpringApplication.run(BookLogApplication.class, args);
